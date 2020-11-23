ad3b078bcb3e80dc9631ade3e97af09f544c312b
KIX-YVR <takato4150@icloud.com>
2020-11-24 00:19:12 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -51,15 +51,6 @@ ActiveRecord::Schema.define(version: 2020_11_13_022656) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_comments_on_item_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
