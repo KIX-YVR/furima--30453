a7a163c2a16ca93035c1f2b1b426467623fb2f19
KIX-YVR <takato4150@icloud.com>
2020-11-30 20:36:52 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :new, :create, :edit, :show, :update, :destroy] do
     resources :purchase, only: [:index, :new, :create]
     resources :buying, only: [:save]
+    resources :comments, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
