b3decdacadf62ee2911e86cf5862bec09459e33c
KIX-YVR <takato4150@icloud.com>
2020-12-05 14:08:47 +0900

    fifth commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_022656) do
+ActiveRecord::Schema.define(version: 2020_12_01_145015) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2020_11_13_022656) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -101,8 +106,6 @@ ActiveRecord::Schema.define(version: 2020_11_13_022656) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "purchase_infos"
-  add_foreign_key "comments", "items"
-  add_foreign_key "comments", "users"
   add_foreign_key "items", "users"
   add_foreign_key "purchase_infos", "items"
   add_foreign_key "purchase_infos", "users"
